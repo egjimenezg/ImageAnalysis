71c97b69996eb91ba0807018749868221d0de514
egjimenezg <egjimenezg@gmail.com>
2014-08-25 01:54:23 -0500

    coding #1 declaring and implementing the functions to open an image

--- ImageAnalysisC/imagewindow.cpp
+++ ImageAnalysisC/imagewindow.cpp
@@ -1,10 +1,53 @@
+#include <QtWidgets>
+
 #include "imagewindow.h"
 
 ImageWindow::ImageWindow(){
+  imageLabel = new QLabel;
+  imageLabel->setBackgroundRole(QPalette::Dark);
+  imageLabel->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Ignored);
+  imageLabel->setMaximumWidth(400);
+  imageLabel->setMaximumHeight(400);
+  imageLabel->setScaledContents(true);
 
+  scrollArea = new QScrollArea;
+  scrollArea->setBackgroundRole(QPalette::Dark);
+  scrollArea->setWidget(imageLabel);
+  setCentralWidget(scrollArea);
+
+  createActions();
+  createMenuBar();
+
+  setWindowTitle(tr("Image Analysis Application"));
+  resize(800,600);
 }
 
-ImageWindow::~ImageWindow()
-{
+ImageWindow::~ImageWindow(){
+}
+
+void ImageWindow::createActions(){
+  openAction = new QAction(tr("Abrir"),this);
+  openAction->setShortcut(tr("Ctrl+A"));
+  connect(openAction, SIGNAL(triggered()), this, SLOT(openImage()));
+}
+
+void ImageWindow::createMenuBar(){
+  fileMenu = new QMenu(tr("&Archivo"),this);
+  fileMenu->addAction(openAction);
+  menuBar()->addMenu(fileMenu);
+}
+
+void ImageWindow::openImage(){
+   QString fileName = QFileDialog::getOpenFileName(this,tr("Abrir Archivo"),QDir::currentPath());
+
+   if(!fileName.isEmpty()){
+     QImage image(fileName);
+     if(image.isNull()){
+         QMessageBox::information(this,tr("Image"),tr("Cannot load %1.").arg(fileName));
+         return;
+     }
+     imageLabel->setPixmap(QPixmap::fromImage(image));
+     imageLabel->adjustSize();
+   }
 
 }
--- ImageAnalysisC/imagewindow.h
+++ ImageAnalysisC/imagewindow.h
@@ -3,6 +3,12 @@
 
 #include <QMainWindow>
 
+class QAction;
+class QLabel;
+class QMenu;
+class QScrollArea;
+class QScrollBar;
+
 class ImageWindow : public QMainWindow
 {
     Q_OBJECT
@@ -10,7 +16,18 @@ class ImageWindow : public QMainWindow
   public:
     ImageWindow();
     ~ImageWindow();
+
+  private slots:
+    void openImage();
+
   private:
+    void createActions();
+    void createMenuBar();
+    QLabel *imageLabel;
+    QScrollArea *scrollArea;
+    QMenu *fileMenu;
+    /*Actions of the menu*/
+    QAction *openAction;
 
 };
 
