5b23ba649ce3ee90d56f04457c1b644879a54dc9
egjimenezg <egjimenezg@gmail.com>
2014-09-09 15:35:43 -0500

    coding #3 adding functions

--- Histograma/mainwindow.h
+++ Histograma/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "qcustomplot.h"
 
 namespace Ui {
 class MainWindow;
@@ -14,14 +15,23 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QVector<double> x;
+    QVector<double> y;
 
 private slots:
     void openImage();
+    void initializePlot();
+    void createHistograma(int i);
+    void initializeVectorValues();
 
 private:
     Ui::MainWindow *ui;
     QImage* image;
+    QCustomPlot *qcustomplot;
+    QCPBars* bars2;
+    QSignalMapper mapper;
     void createActions();
+
 };
 
 #endif // MAINWINDOW_H
