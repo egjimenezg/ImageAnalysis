73ea8354d869d5c7cdb299a3e3c8eb45618bb83a
egjimenezg <egjimenezg@gmail.com>
2014-09-14 23:54:28 -0500

    coding #3 adding Histogram class

---
+++ Histograma/histogram.h
@@ -0,0 +1,21 @@
+#ifndef HISTOGRAM_H
+#define HISTOGRAM_H
+#include "qcustomplot.h"
+#include "image.h"
+
+class Histogram{
+  private:
+     QVector<double> x;
+     QVector<double> frequencies;
+     void initializeVectorValues();
+
+  public:
+    Histogram(QWidget* parent,int,int);
+    ~Histogram();
+    QCustomPlot* qcustomPlot;
+    QCPBars* bars2;
+    void createHistogramForChanelOfTheImage(Image*,char);
+
+};
+
+#endif // HISTOGRAM_H
