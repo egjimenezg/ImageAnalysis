bb3407a81ac583eaf6e12585427863c9523594ba
egjimenezg <egjimenezg@gmail.com>
2014-09-20 22:54:09 -0500

    coding #3 agregando nuevas funciones a la clase imagen

--- Histograma/image.h
+++ Histograma/image.h
@@ -16,7 +16,6 @@ class Image{
     /*Array with the lower values of each channel*/
     int* minValues;
 
-
   public:
     Image();
     ~Image();
@@ -25,11 +24,18 @@ class Image{
     void initializeMinAndMaxValues();
     long getH();
     long getW();
+    int* getMaxVaules();
+    int* getMinValues();
+    int** getRed();
+    void setRed(int**);
+    int** getGreen();
+    void setGreen(int**);
+    int** getBlue();
+    void setBlue(int**);
     int getRedValue(int,int);
     int getGreenValue(int,int);
     int getBlueValue(int,int);
     QImage getRGBImage(char);
-
 
 };
 
