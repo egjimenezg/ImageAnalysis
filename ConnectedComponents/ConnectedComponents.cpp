eba4cd9bf9dabd09e3b82a9d8363e992de6710a2
egjimenezg <egjimenezg@gmail.com>
2015-01-31 20:49:33 -0600

    coding #8 adding example using the depth first search algorithm for the labeling

---
+++ ConnectedComponents/ConnectedComponents.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<stack>
+#define HEIGHT 10
+#define WIDTH 10
+
+using namespace std;
+class Image{
+  private:
+    static const int dx[4];
+    static const int dy[4];
+    static const int image[HEIGHT][WIDTH];
+    int **aux; 
+    int H;
+    int W;
+    void depth_search(long,long,long); 
+  public:
+    Image();
+    ~Image();
+    void printAuxMatrix();
+    void segmentation();
+    
+};
+
+const int Image::dx[4] = {1,0,-1,0};
+const int Image::dy[4] = {0,+1,0,-1}; 
+const int Image::image[HEIGHT][WIDTH] = {{1,1,1,1,1,1,1,1,1,1},
+                                         {1,0,0,0,1,1,1,1,1,1},
+                                         {1,0,0,0,1,1,1,1,1,1},
+                                         {1,1,1,0,1,1,1,1,1,1},
+                                         {1,1,1,0,0,1,1,1,1,1},
+                                         {1,1,1,1,1,1,0,0,1,1},
+                                         {1,1,1,1,1,1,0,0,1,1},
+                                         {1,0,0,1,0,0,1,1,1,1},
+                                         {1,0,0,0,0,0,1,1,1,1},
+                                         {1,1,1,1,1,1,1,1,1,1}};
+
+
+Image::Image(){
+  int i,j;
+  H = HEIGHT;
+  W = WIDTH;
+  aux = new int*[H];
+
+  for(i=0;i<H;i++)
+    aux[i] = new int[W];
+  
+  for(i=0;i<H;i++)
+    for(j=0;j<W;j++)
+      aux[i][j] = 0;
+}
+
+Image::~Image(){
+  for(int i=0;i<H;i++)
+    delete[] aux[i];
+  
+  delete[] aux;
+}
+
+void Image::depth_search(long x,long y,long label){
+  if(x<0 || x==H)
+    return;    
+  if(y<0 || y==W)
+    return;
+  if(aux[x][y] || image[x][y])
+    return;       
+  aux[x][y] = label;
+
+  for(int i=0;i<4;i++) 
+    depth_search(x+dx[i],y+dy[i],label);
+
+}
+
+void Image::printAuxMatrix(){
+  for(int i=0;i<H;i++){
+    for(int j=0;j<W;j++){
+      cout << aux[i][j] << " ";
+    }
+    cout<<endl;
+  }
+}
+
+void Image::segmentation(){
+  int i,j;
+  long label = 1000;
+  for(i=0;i<H;i++){
+    for(j=0;j<W;j++){
+      if(!aux[i][j] && !image[i][j])
+        depth_search(i,j,++label); 
+    } 
+  }
+}
+
+int main() {
+  long label = 1000;
+  Image *image = new Image();
+  image->printAuxMatrix();
+  image->segmentation();
+  cout << endl;
+  image->printAuxMatrix();
+  return 0;
+}
+
+
