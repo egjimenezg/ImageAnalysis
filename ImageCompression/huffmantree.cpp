f1accf50cf8c742fb5857c4ef1016d91c483d69e
egjimenezg <egjimenezg@gmail.com>
2015-02-28 12:28:42 -0600

    coding #10 adding functions for the priority min queue

--- ImageCompression/huffmantree.cpp
+++ ImageCompression/huffmantree.cpp
@@ -1,17 +1,30 @@
 #include "huffmantree.h"
+#include <iostream>
 
 HuffmanTree::HuffmanTree(){}
 
 HuffmanTree::~HuffmanTree(){}
 
-void HuffmanTree::insertIntoQueue(Node* node){
-  int index;
-  while(queue.at(index)){
-   index++;
+void HuffmanTree::insertIntoQueue(Node *node){
+  Node *auxNode = node;
+  queue.push_back(node);
+
+  if(queue.size()>1){
+    vector<Node*>::iterator last = queue.end()-1;
+    vector<Node*>::iterator previous = last-1;
+
+    while(previous >= queue.begin() && (*previous)->getFrequency() > (*last)->getFrequency()){
+      auxNode = (*previous);
+      (*previous) = (*last);
+      (*last) = auxNode;
+      previous--;
+      last--;
+    }
   }
-  queue.insert(index,node);
 }
 
 Node* HuffmanTree::getMin(){
-  return queue.at(0);
+  Node* min = queue.back();
+  queue.pop_back();
+  return min;
 }
